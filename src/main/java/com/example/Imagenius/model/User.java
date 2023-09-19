4c1226c194aff499e7b7a4e1786247f6b570dc60
Dmitrii Derendyaev <dds_stalker@mail.ru>
2023-09-19 12:39:08 +0300

    feature: добавить аннотации для формирования БД с пользователями

--- src/main/java/com/example/Imagenius/model/User.java
+++ src/main/java/com/example/Imagenius/model/User.java
@@ -1,11 +1,24 @@
 package com.example.Imagenius.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
 public class User {
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, unique = true)
+    private Long id;
+    @Column(name = "name", nullable = false)
     private String name;
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
+    @Column(name = "password", nullable = false)
     private String password;
 }
